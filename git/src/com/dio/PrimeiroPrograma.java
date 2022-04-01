7ba5fa2bbac5c22f08a86bcb311dae6563a09f11
Jessica Freitas <102299944+jessicadefreitaz@users.noreply.github.com>
2022-04-01 11:02:59 -0300

    Add files via upload

---
+++ git/src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,22 @@
+package com.dio;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato ();
+		Livro livro = new Livro ();
+		
+		System.out.println(gato);
+		System.out.print(livro);
+		
+		
+
+		
+		/*int a = 2; 
+		int b = 3;		
+		System.out.println("Hello world! " + (a+b) );*/
+
+	}
+
+}
